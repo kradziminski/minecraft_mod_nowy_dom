e42a788e21238299cac3c53e7d44682c0e69c492
K Radziminski <paluszki149@gmail.com>
2015-08-02 20:13:32 +0200

    Zmiana nazwy moda

--- src/main/java/net/minecraft/kamil/mod/ModKamila.java
+++ src/main/java/net/minecraft/kamil/mod/ModKamila.java
@@ -1,8 +1,6 @@
-package com.example.examplemod;
+package net.minecraft.kamil.mod;
 
 
-import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -18,22 +16,20 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ModKamila.MODID, version = ModKamila.VERSION)
+public class ModKamila
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mod_kamila";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-        MinecraftForge.EVENT_BUS.register(new MyEventHandler());
+        MinecraftForge.EVENT_BUS.register(new BudowniczyDomow());
     }
 }
 
-class MyEventHandler {
+class BudowniczyDomow {
     @SubscribeEvent
     public void onPlayerJoin(EntityJoinWorldEvent entityEvent) {
         if (entityEvent.entity instanceof EntityPlayer) {
