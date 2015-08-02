7e929168a1086d93a7a0b7d1de99827880cd7bc4
K Radziminski <paluszki149@gmail.com>
2015-08-02 20:18:53 +0200

    Dodanie proxy dla klienta i serwera

---
+++ src/main/java/net/minecraft/kamil/mod/CommonProxy.java
@@ -0,0 +1,20 @@
+package net.minecraft.kamil.mod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+        MinecraftForge.EVENT_BUS.register(new BudowniczyDomow());
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
--- src/main/java/net/minecraft/kamil/mod/ModKamila.java
+++ src/main/java/net/minecraft/kamil/mod/ModKamila.java
@@ -13,6 +13,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -21,11 +22,14 @@ public class ModKamila
 {
     public static final String MODID = "mod_kamila";
     public static final String VERSION = "1.0";
+
+    @SidedProxy(clientSide="com.bedrockminer.tutorial.ClientProxy", serverSide="com.bedrockminer.tutorial.ServerProxy")
+    public static CommonProxy proxy;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        MinecraftForge.EVENT_BUS.register(new BudowniczyDomow());
+        proxy.init(event);
     }
 }
 
